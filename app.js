a34160cd686f49c8295bbd8849c6234111a845ec
alligatormonday <joey@joeyjepson.com>
2020-10-13 22:06:34 -0700

    Wrote pseudocode to breakdown the inquirer prompts. Started new employee function. Started creating the intern class.

--- app.js
+++ app.js
@@ -10,9 +10,36 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// keep an array of employee objects
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+// Questions to ask:
+// 1: What kind of employee do you want to add?
+// - Choices: Manager, Engineer, Intern
+// - Select: Manager
+// 2: What is their name?
+// 3: What is their ID?
+// 4: What is their email?
+// - If Manager:
+// 5: What is their office number?
+// 6. Would you like to add another employee?
+// -Go back to first question
+// - If Engineer:
+// 5. Enter GitHub username?
+// 6. Would you like to add another employee?
+// - If Intern:
+// 5. Enter school
+// 6. Would you like to add another employee?
+
+// chain .then promises together 
+// prompt function returns a promise
+
+function newEmployee(){
+    
+}
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
